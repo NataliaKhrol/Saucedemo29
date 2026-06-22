a6ce3bd4fe47fe9b4cdde2e82bfdd198c089d262
Natalia Khrol <natalia_khrol@epam.com>
2026-06-22 20:28:06 +0300

    Page object pattern is applied

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,34 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private final By loginInput = By.xpath("//*[@placeholder='Username']");
+    private final By passwordInput = By.xpath("//input[@placeholder='Password']");
+    private final By submitButton = By.cssSelector("#login-button");
+    private final By error = By.xpath("//h3[@data-test='error']");
+    WebDriver driver;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void open() {
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    public void login(String login, String password) {
+        driver.findElement(loginInput).sendKeys(login);
+        driver.findElement(passwordInput).sendKeys(password);
+        driver.findElement(submitButton).click();
+    }
+
+    public boolean isErrorDisplayed() {
+        return driver.findElement(error).isDisplayed();
+    }
+
+    public String getErrorText() {
+        return driver.findElement(error).getText();
+    }
+}
---
+++ src/test/java/tests/LoginTest.java
@@ -0,0 +1,27 @@
+package tests;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void checkLogin() throws InterruptedException {
+        loginPage.open();
+        loginPage.login("standard_user", "secret_sauce");
+        Thread.sleep(5000);
+
+        assertEquals(productsPage.getTitle(), "Products", "Заголовок страницы не соответствует");
+    }
+
+    @Test
+    public void checkIncorrectLogin() {
+        loginPage.open();
+        loginPage.login("", "secret_sauce");
+
+        assertTrue(loginPage.isErrorDisplayed());
+        assertEquals(loginPage.getErrorText(), "Epic sadface: Username is required");
+    }
+}

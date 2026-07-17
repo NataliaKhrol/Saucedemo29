a7afc52ab322a501f155b9ceacbf84c0b2a5e8e3
Natalia Khrol <natalia_khrol@epam.com>
2026-07-17 14:35:13 +0300

    Allure and Lombok are added

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,36 +1,54 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Step;
+import io.qameta.allure.testng.AllureTestNg;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.ITestContext;
 import org.testng.annotations.*;
+import pages.BasketPage;
 import pages.LoginPage;
 import pages.ProductsPage;
+import utils.TestListener;
 
 import java.time.Duration;
 
+@Listeners({AllureTestNg.class, TestListener.class})
 public class BaseTest {
     WebDriver driver;
     LoginPage loginPage;
     ProductsPage productsPage;
+    BasketPage basketPage;
 
+    @Parameters({"browser"})
+    @BeforeMethod
+    public void setUp(@Optional("chrome") String browser, ITestContext context) {
+        if (browser.equalsIgnoreCase("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            options.addArguments("guest");
+            //options.addArguments("headless");
+            driver = new ChromeDriver(options);
+            // driver.manage().window().setSize(new Dimension(1920, 1080));
+        } else if (browser.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            driver = new FirefoxDriver();
+        }
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
+        context.setAttribute("driver", driver);
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
+        basketPage = new BasketPage(driver);
+    }
+
+    @Step("Закрытие")
     @AfterMethod(alwaysRun = true)
     public void close() {
         driver.manage().deleteAllCookies();
         driver.quit();
-    }
-
-    @BeforeMethod
-    public void setUp() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-
-        options.addArguments("guest");
-        options.addArguments("headless");
-        driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(7));
-        // driver.manage().window().setSize(new Dimension(1920, 1080));
-
-        loginPage = new LoginPage(driver);
-        productsPage = new ProductsPage(driver);
     }
 }
